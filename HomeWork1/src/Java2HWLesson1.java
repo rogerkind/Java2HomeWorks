ca67750fee1c8699e433c2d2fd2d2e962f1802f7
rogerkind <gered@mail.ru>
2017-09-01 16:42:29 +0600

    Class Jawa2HWLesson1 finalized

--- HomeWork1/src/Java2HWLesson1.java
+++ HomeWork1/src/Java2HWLesson1.java
@@ -1,2 +1,22 @@
+import course.Course;
+import team.Team;
+import team.members.Animal;
+import team.members.Cat;
+import team.members.Hen;
+import team.members.Hippo;
+
+/**
+ * Class for running Home Work 1 classes and packages
+ *
+ * @author Slava Bugakov
+ * @version 0.0.1 dated 01 Sept 2017
+ */
+
 public class Java2HWLesson1{
+    public static void main(String[] args){
+        Course c = new Course(); // Создаем полосу препятствий
+        Team team = new Team("Zoo", new Animal[]{new Cat("Murzik"), new Hippo("Woofy"), new Hen("Twitty")}); // Создаем команду
+        c.doIt(team); // Просим команду пройти полосу
+        team.showResults(); // Показываем результаты
+    }
 }
