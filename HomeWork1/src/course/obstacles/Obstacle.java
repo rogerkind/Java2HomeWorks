829737f6a775617152a89bc40267bc9eda2ac119
rogerkind <gered@mail.ru>
2017-09-01 16:29:27 +0600

    Finalise "Universal" class Obstacle. Adding docs Clean up

--- HomeWork1/src/course/obstacles/Obstacle.java
+++ HomeWork1/src/course/obstacles/Obstacle.java
@@ -1,31 +1,48 @@
 package course.obstacles;
-import team.members.*;
+
+import team.members.Animal;
+import team.members.Jumpable;
+import team.members.Swimable;
+
+/**
+ * Class - obstacle "universal" class
+ * contains method doIt(Team team) according task requirements
+ * experiment: method pass changes behavior according type of obstacle
+ * witch received from Enumerator Obstacles
+ *
+ * @author Slava Bugakov
+ * @version 0.0.1 dated 31 Aug 2017
+ */
 
 public class Obstacle{
 
-    private Obstacles obstacle;
+    private final Obstacles obstacle;
 
-    Obstacle(Obstacles obstacle){
-        this.obstacle =obstacle;
+    public Obstacle(Obstacles obstacle){
+        this.obstacle = obstacle;
     }
 
     public boolean pass(Animal animal){
         boolean isPassed = false;
-        switch(obstacle){
+        switch(this.obstacle){
             case CROSS:
-                isPassed = animal.run(obstacle.getSize());
+                isPassed = animal.run(this.obstacle.getSize());
                 break;
             case WALL:
-                if (animal instanceof Jumpable)
-                    isPassed = ((Jumpable) animal).jump((float)obstacle.getSize());
+                if(animal instanceof Jumpable) isPassed = ((Jumpable) animal).jump((float) this.obstacle.getSize());
                 else isPassed = false;
                 break;
             case SWIM:
-                if (animal instanceof  Swimable)
-                    isPassed = ((Swimable) animal).swim(obstacle.getSize());
+                if(animal instanceof Swimable) isPassed = ((Swimable) animal).swim(this.obstacle.getSize());
                 else isPassed = false;
                 break;
         }
         return isPassed;
-     }
+    }
+
+    @Override
+    public String toString(){
+        return this.obstacle.toString();
+    }
 }
+
